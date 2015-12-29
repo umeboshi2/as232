be75904bcbc1fee7fa50fe6ae6f8745cbfbf1df3
Joseph Rawson <joseph.rawson.works@littledebian.org>
2015-12-29 11:13:52 -0600

    added project skeleton

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0'
+
+setup(name='as232',
+      version=version,
+      description="python code to help manipulate git-annex",
+      long_description="""\
+python code to help manipulate git-annex""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Joseph Rawson',
+      author_email='joseph.rawson.works@gmail.com',
+      url='https://github.com/umeboshi2/as232',
+      license='License name',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
